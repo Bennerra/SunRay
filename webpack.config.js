9bf28331aef75e0db522e77dfa11c33fb4ad8b38
Nastya <elgina.anasta@yandex.ru>
2024-01-12 19:02:58 +0300

    Changed entry path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.tsx",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),

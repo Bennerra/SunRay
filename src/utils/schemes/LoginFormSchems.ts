a0ad433e205cd7ef49c90168579e6f99fe669f1d
Nastya <elgina.anasta@yandex.ru>
2024-01-18 01:16:27 +0300

    Added validation of new fields to BuyerForm

--- src/utils/schemes/LoginFormSchems.ts
+++ src/utils/schemes/LoginFormSchems.ts
@@ -1,6 +1,10 @@
 import * as yup from "yup";
 
 export const loginFormSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required("Это обязательное поле!")
+    .min(8, "Username должен быть не менее 8 символов"),
   status: yup.string().required("Это обязательное поле!"),
   name: yup.string().required("Это обязательное поле!"),
   lastname: yup.string().required("Это обязательное поле!"),
@@ -8,6 +12,38 @@ export const loginFormSchema = yup.object().shape({
     .string()
     .required("Это обязательное поле!")
     .min(8, "Пароль должен быть не менее 8 символов"),
+  companyName: yup.string().when("status", {
+    is: "seller",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  inn: yup.string().when("status", {
+    is: "seller",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  city: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  street: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  number: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  lat: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  long: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
+  zipcode: yup.string().when("status", {
+    is: "buyer",
+    then: (schema) => schema.required("Это обязательное поле!"),
+  }),
   phone: yup.string().when("status", {
     is: "buyer",
     then: (schema) => schema.required("Это обязательное поле!"),
@@ -16,13 +52,5 @@ export const loginFormSchema = yup.object().shape({
     is: "buyer",
     then: (schema) =>
       schema.required("Это обязательное поле!").email("Некорректный email"),
-  }),
-  companyName: yup.string().when("status", {
-    is: "seller",
-    then: (schema) => schema.required("Это обязательное поле!"),
-  }),
-  inn: yup.string().when("status", {
-    is: "seller",
-    then: (schema) => schema.required("Это обязательное поле!"),
   }),
 });
